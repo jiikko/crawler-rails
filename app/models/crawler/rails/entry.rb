ea94765579d8edc0903d83ad4944d0fbd027cf50
jiikko <n905i.1214@gmail.com>
2017-06-28 09:55:40 +0900

    Impliment Entry#scrape

--- app/models/crawler/rails/entry.rb
+++ app/models/crawler/rails/entry.rb
@@ -1,5 +1,14 @@
+require 'open-uri'
+require 'kconv'
+
 module Crawler::Rails
   class Entry < ApplicationRecord
     belongs_to :page_source
+
+    def scrape
+      file = open(page_source.url)
+      block = eval(scraping_code) # danger!!
+      block.call(Nokogiri::HTML(file.read.toutf8, nil, 'utf-8'))
+    end
   end
 end
