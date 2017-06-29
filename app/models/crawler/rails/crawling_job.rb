47acb927264cd0a7e11817b7df3382ec256d079c
jiikko <n905i.1214@gmail.com>
2017-06-30 00:37:30 +0900

    クロールジョブ一覧にlast status を表示する

--- app/models/crawler/rails/crawling_job.rb
+++ app/models/crawler/rails/crawling_job.rb
@@ -10,10 +10,9 @@ inner join (
   select max(id) as id
   from crawler_rails_crawling_jobs
   group by site_id
-) c
-on crawler_rails_crawling_jobs.id = c.id
+) c on crawler_rails_crawling_jobs.id = c.id
       SQL
-      Crawler::Rails::CrawlingJob.joins(join_sql)
+      Crawler::Rails::CrawlingJob.joins(join_sql).select(:id, :site_id, :created_at)
     }
   end
 end
