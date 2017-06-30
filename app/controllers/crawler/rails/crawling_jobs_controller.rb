7879dd40d3bd1cc8958a28dca6f6f09f4ba1f082
jiikko <n905i.1214@gmail.com>
2017-06-30 20:45:53 +0900

    クロールジョブ一覧にて日付検索機能を実装する

--- app/controllers/crawler/rails/crawling_jobs_controller.rb
+++ app/controllers/crawler/rails/crawling_jobs_controller.rb
@@ -1,27 +1,41 @@
 module Crawler
   module Rails
     class CrawlingJobsController < ApplicationController
+      helper_method :view_type_time_series, :view_type_latests_per_site
+
       def index
         if params[:view_type].nil?
-          redirect_to(crawling_jobs_path(view_type: 'latests_per_site'))
+          redirect_to(crawling_jobs_path(view_type: view_type_latests_per_site))
           return
         end
+        params[:start_on] ||= Date.today
+        params[:end_on] ||= Date.today + 1.day
 
         @failure_crawling_jobs, @success_crawling_jobs =
           case params[:view_type]
-          when 'latests_per_site'
+          when view_type_latests_per_site
             @h2_text = 'サイト毎最新のクロール'
             [CrawlingJob.latests_per_site.failure, CrawlingJob.latests_per_site.success]
-          when 'day'
+          when view_type_time_series
             @h2_text = '日付でクロール'
-            [CrawlingJob.failure, CrawlingJob.success]
+            [ CrawlingJob.failure.where!(created_at: params[:start_on]..params[:end_on]),
+              CrawlingJob.success.where!(created_at: params[:start_on]..params[:end_on])
+            ]
           else
-            [CrawlingJob.failure, CrawlingJob.success]
+            raise('not found view_type')
           end
       end
 
       def show
         @crawling_job = CrawlingJob.find(params[:id])
+      end
+
+      def view_type_latests_per_site
+        'latests_per_site'
+      end
+
+      def view_type_time_series
+        'time_series'
       end
     end
   end
